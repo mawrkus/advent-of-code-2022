7445e9a6ee362e84085e2628157c252bdbe167c0
mawrkus <web@sparring-partner.be>
2022-12-04 15:11:57 +0100

    Changed some HTTP header

--- scripts/helpers/fetchPuzzle.js
+++ scripts/helpers/fetchPuzzle.js
@@ -15,7 +15,7 @@ export function fetchPuzzle(dayNumber, partNumber) {
     headers: {
       cookie: process.env.ADVENT_COOKIE,
       "user-agent": "mawrkus/1.0",
-      "x-whomai": "just an automated bot created by @crossrecursion on Twitter",
+      "x-whomai": "just an automated bot created by @crossrecursion",
       "x-purpose":
         "fetching the puzzles description of the day to generate a nice README file for my creator's repo in GitHub",
       "x-thanks": "thank you for building Advent of Code, I owe you my life!",
