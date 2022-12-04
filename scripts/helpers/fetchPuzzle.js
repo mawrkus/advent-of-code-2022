efb9a25885aa957726883d680cc7ee892a9c129c
mawrkus <web@sparring-partner.be>
2022-12-04 13:11:22 +0100

    Improve scripts

--- scripts/helpers/fetchPuzzle.js
+++ scripts/helpers/fetchPuzzle.js
@@ -14,6 +14,11 @@ export function fetchPuzzle(dayNumber, partNumber) {
   return fetch(`https://adventofcode.com/2022/day/${dayNumber}`, {
     headers: {
       cookie: process.env.ADVENT_COOKIE,
+      "user-agent": "mawrkus/1.0",
+      "x-whomai": "just an automated bot created by @crossrecursion on Twitter",
+      "x-purpose":
+        "fetching the puzzles description of the day to generate a nice README file for my creator's repo in GitHub",
+      "x-thanks": "thank you for building Advent of Code, I owe you my life!",
     },
   })
     .then((response) => response.text())
@@ -23,7 +28,9 @@ export function fetchPuzzle(dayNumber, partNumber) {
       const html = $("main article.day-desc")
         .eq(partNumber - 1)
         .html()
+        .trim()
         .replace(/<\/p>/g, "\n")
+        .replace(/<ul>[^<]*/g, "")
         .replace(/<li>/g, "- ")
         .replace(/<code><em>/g, "**`")
         .replace(/<\/em>.*<\/code>/g, "`**")
@@ -32,7 +39,7 @@ export function fetchPuzzle(dayNumber, partNumber) {
         .replace(/(<code>|<\/code>)/g, "`")
         .replace(/(<em[^>]*>|<\/em>)/g, "**");
 
-      const text = load(html).text();
+      const text = load(html).text().trim();
 
       if (partNumber === 1) {
         const [title, puzzleName] = text.match(/--- Day.+: (.+) ---/);
--- templates/README.md
+++ templates/README.md
@@ -1,6 +1,6 @@
 # 🎄 Day {dayNumber}: {puzzleName}
 
-💡 https://adventofcode.com/2022/day/{dayNumber}
+💡 [https://adventofcode.com/2022/day/{dayNumber}](https://adventofcode.com/2022/day/{dayNumber})
 
 ## 🧩 First puzzle
 
